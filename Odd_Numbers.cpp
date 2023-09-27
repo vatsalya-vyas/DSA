46ae2873c9bd34676e5c77ec0c6e7531069f775d
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-09-27 21:22:23 +0530

    Odd Numbers Code

---
+++ Odd_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 9;
+    if (number % 2 != 0)
+    {
+        cout << "The number is odd." << endl;
+    }
+    else
+    {
+        cout << "The number is even." << endl;
+    }
+    return 0;
+}
