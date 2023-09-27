2c08816ed23ecd8e26094d4a6eb6c05494d7644b
Vatsalya Vyas <140804211+vatsalya-vyas@users.noreply.github.com>
2023-09-27 21:13:47 +0530

    Greatest Common Divisor Code Modified

--- Greatest_Common_Divisor.cpp
+++ Greatest_Common_Divisor.cpp
@@ -14,7 +14,7 @@ int gcd(int a, int b)
 
 int main()
 {
-    int num1 = 24;
+    int num1 = 27;
     int num2 = 36;
     int result = gcd(num1, num2);
     cout << "GCD of " << num1 << " and " << num2 << " is: " << result << endl;
